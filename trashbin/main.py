b905184f148e72670319295aa6fe895968913bae
Mouser5 <149000034+Mouser5@users.noreply.github.com>
2025-06-20 00:12:09 +0300

    Add files via upload

---
+++ trashbin/main.py
@@ -0,0 +1,6 @@
+from realized import StaticSort
+
+if __name__ == "__main__":
+    sorter = StaticSort()
+    data = [5, 3, 2, 1, 4]
+    print("Sorted:", sorter.sort_method_1(data))
--- trashbin/test_hidden.py
+++ trashbin/test_hidden.py
@@ -1,147 +1,57 @@
 import unittest
 from realized import StaticSort
 
-class TestStaticSortMethod1 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([]), [])
-    def test_duplicate_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_1(self):
-        self.assertEqual(self.sorter.sort_method_1([1]), [1])
+class BaseTestClass (unittest.TestCase):
+    sort_method_name=None
 
-class TestStaticSortMethod2 (unittest.TestCase):
     def setUp(self):
+        if self.sort_method_name is None:
+            self.skipTest("Base test class – no sort method defined")
         self.sorter = StaticSort()
-    def test_sorted_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([]), [])
-    def test_duplicate_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_2(self):
-        self.assertEqual(self.sorter.sort_method_2([1]), [1])
 
-class TestStaticSortMethod3 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([]), [])
-    def test_duplicate_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_3(self):
-        self.assertEqual(self.sorter.sort_method_3([1]), [1])
+    def get_name(self):
+        return getattr(self.sorter, self.sort_method_name)
+    def test_sorted_sort_method(self):
+        self.assertEqual(self.get_name()([1,2,3]),[1,2,3])
+    def test_unsorted_sort_method(self):
+        self.assertEqual(self.get_name()([3, 2, 1]), [1, 2, 3])
+    def test_null_sort_method(self):
+        self.assertEqual(self.get_name()([]), [])
+    def test_duplicate_sort_method(self):
+        self.assertEqual(self.get_name()([3, 2, 1, 3]), [1, 2, 3, 3])
+    def test_negative_sort_method(self):
+        self.assertEqual(self.get_name()([1, -2, 3]), [-2, 1, 3])
+    def test_one_number_sort_method(self):
+        self.assertEqual(self.get_name()([1, 1, 1]), [1, 1, 1])
+    def test_only_one_sort_method(self):
+        self.assertEqual(self.get_name()([1]), [1])
+    # def test_not_int_sort_method(self):
+    #     self.assertEqual(self.get_name()(['132']),"Error")
+    #def test_list_of_string_sort_method(self):
+    #    self.assertEqual(self.get_name()(['3','2','1']),"Error")
 
-class TestStaticSortMethod4 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([]), [])
-    def test_duplicate_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_4(self):
-        self.assertEqual(self.sorter.sort_method_4([1]), [1])
 
-class TestStaticSortMethod5 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([]), [])
-    def test_duplicate_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_5(self):
-        self.assertEqual(self.sorter.sort_method_5([1]), [1])
 
-class TestStaticSortMethod6 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([]), [])
-    def test_duplicate_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_6(self):
-        self.assertEqual(self.sorter.sort_method_6([1]), [1])
+class TestStaticSortMethod1 (BaseTestClass, unittest.TestCase):
+   sort_method_name = "sort_method_1"
 
-class TestStaticSortMethod7 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([]), [])
-    def test_duplicate_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_7(self):
-        self.assertEqual(self.sorter.sort_method_7([1]), [1])
+class TestStaticSortMethod2 (BaseTestClass, unittest.TestCase):
+    sort_method_name = "sort_method_2"
 
-class TestStaticSortMethod8 (unittest.TestCase):
-    def setUp(self):
-        self.sorter = StaticSort()
-    def test_sorted_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([1,2,3]),[1,2,3])
-    def test_unsorted_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([3, 2, 1]), [1, 2, 3])
-    def test_null_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([]), [])
-    def test_duplicate_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([3, 2, 1, 3]), [1, 2, 3, 3])
-    def test_negative_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([1, -2, 3]), [-2, 1, 3])
-    def test_one_number_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([1, 1, 1]), [1, 1, 1])
-    def test_only_one_sort_method_8(self):
-        self.assertEqual(self.sorter.sort_method_8([1]), [1])
+class TestStaticSortMethod3 (BaseTestClass, unittest.TestCase):
+    sort_method_name = "sort_method_3"
 
+class TestStaticSortMethod4 (BaseTestClass, unittest.TestCase):
+    sort_method_name = "sort_method_4"
+
+class TestStaticSortMethod5 (BaseTestClass, unittest.TestCase):
+    sort_method_name = "sort_method_5"
+
+class TestStaticSortMethod6 (BaseTestClass, unittest.TestCase):
+   sort_method_name = "sort_method_6"
+
+class TestStaticSortMethod7 (BaseTestClass, unittest.TestCase):
+   sort_method_name = "sort_method_7"
+
+class TestStaticSortMethod8 (BaseTestClass, unittest.TestCase):
+    sort_method_name = "sort_method_8"
